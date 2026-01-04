67d89af070817b0e8666249ef7d1f7fd1fba8ab6
santiagobedon <santiago.bedon@correounivalle.edu.co>
2026-01-03 21:37:21 -0500

    correcion de cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,15 @@ import ventasRapidasRoutes from './routes/ventasRapidas.routes'
 
 const app = express();
 
-app.use(cors());
+
+// Configuración específica
+app.use(cors({
+  origin: 'http://localhost:5173', // URL de tu frontend Vite
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
