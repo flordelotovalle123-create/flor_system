862bd2a84d02f2221c0f9c82828d7817bee445b6
santiagobedon <santiago.bedon@correounivalle.edu.co>
2026-01-04 20:06:56 -0500

    correccion de cors

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:5173', // URL de tu frontend Vite
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
